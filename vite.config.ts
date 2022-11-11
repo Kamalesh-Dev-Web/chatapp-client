677d989cc628b5f5a5a61aca43b6eb30718a808e
Kamalesh-Dev-Web <kamalsurea619@gmail.com>
2022-11-11 19:39:56 +0530

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+import { fileURLToPath } from "url";
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
